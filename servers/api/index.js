ac27fb0aa5653214e36793799d5da74d684f3c64
pavanmanishd <pavanmanishd@gmail.com>
2023-11-04 22:35:21 +0530

    types route

--- servers/api/index.js
+++ servers/api/index.js
@@ -1,11 +1,12 @@
-// const express = require('express');
 import express from "express";
 import { createClient } from "@libsql/client";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 
 const client = createClient({
     url: process.env.DATABASE_URL,
@@ -13,9 +14,15 @@ const client = createClient({
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+    res.send('Hello World');
+});
+
+app.get('/types', async (req, res) => {
+    const categories = await client.execute("SELECT distinct type FROM signs");
+    const types = categories.rows.map((row) => row.type);
+    res.json(types);
 });
 
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+    console.log('Server started on port 3000');
 });
