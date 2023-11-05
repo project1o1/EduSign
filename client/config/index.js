e1a82e4c7bbaff13175276400d80facefa060660
pavanmanishd <pavanmanishd@gmail.com>
2023-11-05 21:41:18 +0530

    Update REACT_APP_CLERK_PUBLISHABLE_KEY in development variables

--- client/config/index.js
+++ client/config/index.js
@@ -1,7 +1,7 @@
 const variables = {
     development: {
         API_URL: 'http://localhost:3000',
-        REACT_APP_CLERK_PUBLISHABLE_KEY: "pk_test_Z2xvcmlvdXMtdmVydmV0LTcxLmNsZXJrLmFjY291bnRzLmRldiQ"
+        REACT_APP_CLERK_PUBLISHABLE_KEY: "pk_test_bHVja3ktaGVuLTc0LmNsZXJrLmFjY291bnRzLmRldiQ"
     },
     production: {
         API_URL: ''
