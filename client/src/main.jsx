5f75ec8d354c03fbda4bfb642640223299894a0a
Vishal0129 <vishalsai010304@gmail.com>
2023-11-06 22:51:09 +0530

    Commented out unused code and added BrowserRouter to render App component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,27 +1,12 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
-import {
-  ClerkProvider,
-  SignedIn,
-  SignedOut,
-  UserButton,
-  useUser,
-  RedirectToSignIn,
-} from "@clerk/clerk-react";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <ClerkProvider publishableKey={clerkPubKey}>
-  //   <SignedIn>
-  //       {/* <App /> */}
-  //       <h1>Hello</h1>
-  //   </SignedIn>
-  //   <SignedOut>
-  //     <RedirectToSignIn />
-  //   </SignedOut>
-  // </ClerkProvider>
   <BrowserRouter>
+
   <App />
   </BrowserRouter>
 );
