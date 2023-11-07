c75d25ecde3552c00b189eac5f831b08bd7aa103
Vishal0129 <vishalsai010304@gmail.com>
2023-11-07 11:31:23 +0530

    Refactor PreviousTests component to fetch and display previous test stats

--- client/src/components/PreviousTests.jsx
+++ client/src/components/PreviousTests.jsx
@@ -7,48 +7,57 @@ import { v4 as uuidv4 } from "uuid";
 function PreviousTests() {
   const [previousTests, setPreviousTests] = useState(null);
   const api = "http://localhost:3000";
-    const { user } = useUser();
+  const { user } = useUser();
 
-useEffect(() => {
-    axios.get(api + "/stats/test/" + user.username)
-        .then((response) => {
-            // console.log(response.data);
-            setPreviousTests(response.data);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-}, []);
+  useEffect(() => {
+    axios
+      .get(api + "/stats/test/" + user.username)
+      .then((response) => {
+        // console.log(response.data);
+        setPreviousTests(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   // previousTests.dateStats => data : tests, accuracy, type
+  const renderPreviousTests = () => {
+    let ptc = [];
+    Object.keys(previousTests.dateStats)
+      .slice(0, 5)
+      .map((date) => {
+        ptc.push(
+          <div className="previous-test" key={uuidv4()}>
+            <h3>{date}</h3>
+            <div className="previous-test-stats">
+              <div className="previous-test-stat">
+                <h4>Tests</h4>
+                <p>{previousTests.dateStats[date].tests}</p>
+              </div>
+              <div className="previous-test-stat">
+                <h4>Accuracy</h4>
+                <p>{previousTests.dateStats[date].accuracy}%</p>
+              </div>
+              <div className="previous-test-stat">
+                <h4>Type</h4>
+                <p>{previousTests.dateStats[date].type}</p>
+              </div>
+            </div>
+          </div>
+        );
+      });
+    return ptc;
+  };
+
   return (
     <div className="previous-tests">
       <h2>Previous Tests</h2>
-      {previousTests && <div className="previous-tests-list">
-        {
-            Object.keys(previousTests.dateStats).map((date) => (
-                <div className="previous-test" key={uuidv4()}>
-                    <h3>{date}</h3>
-                    <div className="previous-test-stats">
-                        <div className="previous-test-stat">
-                            <h4>Tests</h4>
-                            <p>{previousTests.dateStats[date].tests}</p>
-                        </div>
-                        <div className="previous-test-stat">
-                            <h4>Accuracy</h4>
-                            <p>{previousTests.dateStats[date].accuracy}%</p>
-                        </div>
-                        <div className="previous-test-stat">
-                            <h4>Type</h4>
-                            <p>{previousTests.dateStats[date].type}</p>
-                        </div>
-                    </div>
-                </div>
-            ))
-        }
-      </div>}
+      {previousTests && (
+        <div className="previous-tests-list">{renderPreviousTests()}</div>
+      )}
     </div>
   );
 }
-
 
 export default PreviousTests;
