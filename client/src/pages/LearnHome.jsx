f34bbb5fb7a55d684cf250d42b4a7fb8fe03d264
pavanmanishd <pavanmanishd@gmail.com>
2023-11-07 10:37:01 +0530

    Refactor LearnHome component to improve UI and add progress bar

--- client/src/pages/LearnHome.jsx
+++ client/src/pages/LearnHome.jsx
@@ -10,6 +10,7 @@ const LearnHome = () => {
   const [completedStatus, setCompletedStatus] = useState({});
   const { user } = useUser();
   const navigate = useNavigate();
+
   useEffect(() => {
     fetch(`${api}/signs/${type}`)
       .then((res) => res.json())
@@ -42,56 +43,123 @@ const LearnHome = () => {
     updateCompletedStatus();
   }, [words, type, user]);
 
-  const handleClick = (word) => {
+  const handleWordClick = (word) => {
     console.log("Selected word:", word);
     navigate(`/learn/${type}/${word.name}`);
   };
 
-  const wordList = words.map((word) => {
+  const renderWordCards = () => {
+    const cardStyle = {
+      border: "1px solid #ced4da",
+      borderRadius: "8px",
+      padding: "10px",
+      margin: "10px 0",
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      backgroundColor: "#f5f5f5",
+      transition: "transform 0.3s",
+    };
+  
+    const completedCardStyle = {
+      ...cardStyle,
+      backgroundColor: "#c3e6cb",
+    };
+  
+    const cardHoverStyle = {
+      transform: "scale(1.05)",
+      cursor: "pointer",
+    };
+  
+    return words.map((word) => (
+      <div
+        key={word.id}
+        onClick={() => handleWordClick(word)}
+        style={completedStatus[word.name] ? completedCardStyle : cardStyle}
+        onMouseOver={(e) => (e.currentTarget.style.transform = "scale(1.01)")}
+        onMouseOut={(e) => (e.currentTarget.style.transform = "scale(1)")}
+      >
+        <p style={{ fontSize: "1.2rem" }}>{word.name}</p>
+        {completedStatus[word.name] ? (
+          <span style={{ fontSize: "1.2rem", color: "#155724" }}>&#10003;</span>
+        ) : null}
+      </div>
+    ));
+  };
+  
+
+  const calculateProgress = () => {
+    const completedCount = Object.values(completedStatus).filter(
+      (status) => status
+    ).length;
+    const totalCount = Object.values(completedStatus).length;
+    return {
+      completedCount,
+      totalCount,
+      percentage: (completedCount / totalCount) * 100,
+    };
+  };
+
+  const renderProgressBar = () => {
+    const { completedCount, totalCount, percentage } = calculateProgress();
+
+    const progressContainerStyle = {
+      marginTop: "20px",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    };
+
+    const progressBarStyle = {
+      width: "100%",
+      height: "30px",
+      backgroundColor: "#f5f5f5",
+      borderRadius: "5px",
+      overflow: "hidden",
+    };
+
+    const progressStyle = {
+      height: "100%",
+      width: `${percentage}%`,
+      backgroundColor: "#4caf50",
+      borderRadius: "5px",
+    };
+
+    const completionMessageStyle = {
+      display: "flex",
+      justifyContent: "flex-end",
+      alignItems: "center",
+      marginTop: "10px",
+      color: completedCount === totalCount ? "#155724" : "#495057",
+    };
+
     return (
-      <div key={word.id} onClick={() => handleClick(word)}>
-        <p>{word.name}</p>
-        {completedStatus[word.name] ? <p>Completed</p> : <p>Incomplete</p>}
+      <div style={progressContainerStyle}>
+        <h2 style={{ color: "#212529", marginBottom: "10px" }}>Progress</h2>
+        <div style={progressBarStyle}>
+          <div style={progressStyle} />
+        </div>
+        <p style={{ marginTop: "10px", color: "#495057" }}>
+          {completedCount} / {totalCount}
+        </p>
+        {completedCount === totalCount && (
+          <div style={completionMessageStyle}>
+            <span style={{ marginRight: "10px" }}>🎉</span>
+            <span>
+              Congratulations! You have mastered all the lessons in this category.
+            </span>
+          </div>
+        )}
       </div>
     );
-  });
-
-  const completedCount = Object.values(completedStatus).filter(
-    (status) => status
-  ).length;
-  const totalCount = Object.values(completedStatus).length;
-  const progress = (
-    <div>
-      <h2>Progress</h2>
-      <p>
-        {completedCount} / {totalCount}
-      </p>
-      <div
-        style={{
-          border: "1px solid #ccc",
-          width: "100%",
-          backgroundColor: "#f3f3f3",
-          borderRadius: "5px",
-        }}
-      >
-        <div
-          style={{
-            height: "20px",
-            width: `${(completedCount / totalCount) * 100}%`,
-            backgroundColor: "#4caf50",
-            borderRadius: "5px",
-          }}
-        ></div>
-      </div>
-    </div>
-  );
+  };
 
   return (
     <div>
-      <h1>Learn</h1>
-      <h2>{type}</h2>
-      {progress}
-      {wordList}
+      <h1 style={{ color: "#212529" }}>Learn</h1>
+      <h2 style={{ color: "#212529" }}>{type}</h2>
+      {renderProgressBar()}
+      {renderWordCards()}
     </div>
   );
 };
